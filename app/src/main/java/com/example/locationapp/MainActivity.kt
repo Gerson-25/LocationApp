5f2c755e0d51666b3c892ba15b82300033a7d9c8
Gerson Beltran <gmisaelbeltran@gmail.com>
2020-01-16 18:02:58 -0600

    adding control Button

--- app/src/main/java/com/example/locationapp/MainActivity.kt
+++ app/src/main/java/com/example/locationapp/MainActivity.kt
@@ -2,11 +2,21 @@ package com.example.locationapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.example.locationapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val biding: ActivityMainBinding=DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        biding.button.setOnClickListener{
+            Toast.makeText(this, "obteniendo la ubicacion...", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
+
 }
